9dcf3a01dc6985790ecc84a4a2d6158ae07df528
qidCoder <qidcoder@gmail.com>
2020-09-21 08:37:16 -0400

    Create arraysPredict.js

---
+++ arraysPredict.js
@@ -0,0 +1,54 @@
+//Created by Shelley Ophir
+//Coding Dojo Sep. 21,2020
+//go through the following code snippets and predict the output
+
+//Predict 1
+
+var arr = [8,6,7,5,3,0,9]
+for(var i = 0; i < arr.length; i++){    
+    console.log(arr[i]);
+}
+
+//predict 1 prediction
+//8,6,7,5,3,0,9
+
+//////////////////////////////////////////////////////////////
+
+//Predict 2
+
+var arr = [7,3,8,4,2,0,1];
+for(var i = 0; i < arr.length; i++){ 
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    } 
+    else{
+        console.log("That is odd!");
+    }
+}
+
+//predict 2 output
+//That is odd!, That is odd!, 8, 4, 2, 0, That is odd!
+
+///////////////////////////////////////////////////////////////
+
+//Predict 3
+
+var arr = [1,3,8,-5,0,-2,4,-1];
+var newArr = [];
+for(var i = 0; i< arr.length; i++){
+    if(arr[i] < 0){
+        newArr.push(arr[i]);
+        arr[i] = arr[i] * -1;
+    }
+    else if(arr[i] == 0){
+        arr[i] = "Zero";
+    }
+    else{
+        arr[i] = arr[i] * -1;
+    }
+}
+console.log(arr);
+console.log(newArr);
+
+//predict 3 prediction
+//[-1,-3,-8,5,"Zero",2,-4,1],[-5,-2,-1]
